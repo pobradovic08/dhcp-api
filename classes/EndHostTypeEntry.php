235cc28dcfee898d013399736d50b9f310ae6c4b
Pavle Obradovic <pobradovic08@gmail.com>
2016-12-30 21:50:50 +0100

    Unit test for EndHostTypeEntry

--- classes/EndHostTypeEntry.php
+++ classes/EndHostTypeEntry.php
@@ -2,28 +2,38 @@
 
 class EndHostTypeEntry {
 
-  private $id;
-  private $description;
+    private $id;
+    private $description;
 
-  public function __construct(array $data) {
-    if(isset($data['end_host_type_id'])){
-      $this->id = $data['end_host_type_id'];
-      $this->description  = $data['end_host_type_description'];
+    public function __construct (array $data) {
+        if (isset($data['end_host_type_id'])) {
+            if((int) $data['end_host_type_id'] <= 0  or !is_int($data['end_host_type_id'])){
+                throw new InvalidArgumentException("ID invalid");
+            }
+            $this->id = (int) $data['end_host_type_id'];
+        }
+        if(!isset($data['end_host_type_description'])){
+            throw new InvalidArgumentException("Missing required data.");
+        }
+        if(strlen($data['end_host_type_description']) <= 64) {
+            $this->description = (string)$data['end_host_type_description'];
+        }else{
+            throw new InvalidArgumentException("Description too long");
+        }
     }
-  }
 
-  public function getId() {
-    return $this->id;
-  }
+    public function getId () {
+        return $this->id;
+    }
 
-  public function getDescription() {
-    return $this->description;
-  }
+    public function getDescription () {
+        return $this->description;
+    }
 
-  public function serialize () {
-    return [
-      'end_host_type_id' => $this->getId(),
-      'end_host_type_description' => $this->getDescription(),    
-    ];
-  }
+    public function serialize () {
+        return [
+            'end_host_type_id' => $this->getId (),
+            'end_host_type_description' => $this->getDescription (),
+        ];
+    }
 }
