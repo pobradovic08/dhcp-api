5fbf54b915b39b8a41607de627ea742e3f40cb03
Pavle Obradovic <pobradovic08@gmail.com>
2017-01-02 19:57:48 +0100

    - Validator unit tests and bug fixes

--- classes/Validator.php
+++ classes/Validator.php
@@ -8,19 +8,29 @@
  */
 class Validator {
 
+    /*
+     * ID must be integer between 1 and infinity
+     */
     static function validateId ($id) {
         return is_int ($id) and $id > 0;
     }
 
+    /*
+     * VLAN ID must be integer between 1 and 4094
+     * 0 and 4095 are reserved
+     */
     static function validateVlanId ($vlan_id) {
         return is_int ($vlan_id) and $vlan_id > 0 and $vlan_id < 4095;
     }
 
     static function validateIpAddress ($ip) {
-        return filter_var($ip, FILTER_VALIDATE_IP);
+        return boolval(filter_var($ip, FILTER_VALIDATE_IP));
     }
 
     static function validateIpMask ($mask) {
+        if(!self::validateIpAddress($mask)){
+            return false;
+        }
         $dec = ip2long($mask);
         /*
          * If mask is 0.0.0.0 ip2long will return 0 which is evaluated to false
@@ -29,12 +39,15 @@ class Validator {
         if($dec or $mask == '0.0.0.0'){
             $bin = decbin($dec);
             // For decbin(0) result is just "0" so /^0$/
-            return preg_match('/(^0$|^1+0*$)/', $bin);
+            return boolval(preg_match('/(^0$|^1+0*$)/', $bin));
         }
         return false;
     }
 
+    /*
+     * String must be 64 or less characters
+     */
     static function validateDescription ($description){
-        return strlen($description) <= 64;
+        return strlen($description) <= 64 and strlen($description) > 0;
     }
 }
---
+++ tests/classes/ValidatorTest.php
@@ -0,0 +1,154 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: pajaja
+ * Date: 1/2/2017
+ * Time: 5:33 PM
+ */
+class ValidatorTest extends PHPUnit_Framework_TestCase {
+
+    public function validIds () {
+        return [
+            [1], [2], [41], [25], [22], [999], [555], [65535], [65536]
+        ];
+    }
+
+    public function validVlanIds () {
+        return [
+            [1], [2], [41], [25], [22], [4094]
+        ];
+    }
+
+    public function invalidIds () {
+        return [
+            [-1], [0], ["asd"], [true], [false], [[]], [new ArrayObject()]
+        ];
+    }
+
+    public function invalidVlanIds () {
+        return [
+            [-1], [0], [4095], [9999], ["asd"], [true], [false], [[]], [new ArrayObject()]
+        ];
+    }
+
+    public function validIps () {
+        return [
+            ['1.1.1.1'], ['1.1.1.0'], ['0.0.0.0'], ['0.1.1.1'], ['255.255.255.255']
+        ];
+    }
+
+    public function invalidIps () {
+        return [
+            ['1.1.1.256'], ['1.1.1.'], [169090815], [-4125876481], ['asd']
+        ];
+    }
+
+    public function validMasks () {
+        return [['255.255.255.255'], ['255.255.255.254'], ['255.255.255.252'], ['255.255.255.248'],
+            ['255.255.255.240'], ['255.255.255.224'], ['255.255.255.192'], ['255.255.255.128'],
+            ['255.255.255.0'], ['255.255.254.0'], ['255.255.252.0'], ['255.255.248.0'],
+            ['255.255.240.0'], ['255.255.224.0'], ['255.255.192.0'], ['255.255.128.0'],
+            ['255.255.0.0'], ['255.254.0.0'], ['255.252.0.0'], ['255.248.0.0'], ['255.240.0.0'],
+            ['255.224.0.0'], ['255.192.0.0'], ['255.128.0.0'], ['255.0.0.0'], ['254.0.0.0'],
+            ['252.0.0.0'], ['248.0.0.0'], ['240.0.0.0'], ['224.0.0.0'], ['192.0.0.0'], ['128.0.0.0']];
+    }
+
+    public function invalidMasks () {
+        return [
+            ['255.255.255.253'], [169090815], [-4125876481], [0], ['asd'], [false], [true]
+        ];
+    }
+
+    public function validDescription () {
+        return [
+            ['Test description'], ['TestdescriptionTestdescriptionTestdescriptionTestdescriptionTest'],
+            ['Test deskripšn'], ['???? ?????????']
+        ];
+    }
+
+    public function invalidDescription () {
+        return [
+            [''], ['TestdescriptionTestdescriptionTestdescriptionTestdescriptionTestd']
+        ];
+    }
+
+    /**
+     * @dataProvider validIds
+     */
+    public function testValidIds ($id) {
+        $this->assertTrue (Validator::validateId ($id));
+    }
+
+    /**
+     * @dataProvider invalidIds
+     * @param $id
+     */
+    public function testInvalidIds ($id) {
+        $this->assertFalse (Validator::validateId ($id));
+    }
+
+    /**
+     * @dataProvider validVlanIds
+     * @param $vlan
+     */
+    public function testValidVlanIds ($vlan) {
+        $this->assertTrue (Validator::validateVlanId ($vlan));
+    }
+
+    /**
+     * @dataProvider invalidVlanIds
+     * @param $vlan
+     */
+    public function testInvalidVlanIds ($vlan) {
+        $this->assertFalse (Validator::validateVlanId ($vlan));
+    }
+
+    /**
+     * @dataProvider validIps
+     * @param $ip
+     */
+    public function testValidIpAddresses ($ip) {
+        $this->assertTrue (Validator::validateIpAddress ($ip));
+    }
+
+    /**
+     * @dataProvider invalidIps
+     * @param $ip
+     */
+    public function testInvalidIpAddresses ($ip) {
+        $this->assertFalse (Validator::validateIpAddress ($ip));
+    }
+
+    /**
+     * @dataProvider validMasks
+     * @param $mask
+     */
+    public function testValidMasks ($mask) {
+        $this->assertTrue (Validator::validateIpMask ($mask));
+    }
+
+    /**
+     * @dataProvider invalidMasks
+     * @param $mask
+     */
+    public function testInvalidMasks ($mask) {
+        $this->assertFalse (Validator::validateIpMask ($mask));
+    }
+
+    /**
+     * @dataProvider validDescription
+     * @param $d
+     */
+    public function testValidDescriptions ($d) {
+        $this->assertTrue (Validator::validateDescription ($d));
+    }
+
+    /**
+     * @dataProvider invalidDescription
+     * @param $d
+     */
+    public function testInvalidDescriptions ($d) {
+        $this->assertFalse (Validator::validateDescription ($d));
+    }
+}
