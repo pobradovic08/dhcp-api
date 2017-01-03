057e3fc3f5d9a3c42cf84266833cab30bb90faae
Pavle Obradovic <pobradovic08@gmail.com>
2017-01-03 19:10:24 +0100

    Routes - terse option for all routes

--- public/index.php
+++ public/index.php
@@ -94,15 +94,17 @@ $app->group ('/reservations', function () use ($app) {
     /* Get all reservations */
     $app->get ('/[{mode:terse}]', '\ReservationController:get_reservations');
     /* Get all reservations from specific subnet */
-    $app->get ('/subnet/{subnet_id:[0-9]+}', '\ReservationController:get_reservations_for_subnet');
+    $app->get ('/subnet/{subnet_id:[0-9]+}[/{mode:terse}]', '\ReservationController:get_reservations_for_subnet');
     /* Get all reservations from specific group */
-    $app->get ('/group/{group_id:[0-9]+}', '\ReservationController:get_reservations_for_group');
+    $app->get ('/group/{group_id:[0-9]+}[/{mode:terse}]', '\ReservationController:get_reservations_for_group');
     /* Get specific reservation by ID */
     $app->get ('/id/{id:[0-9]+}[/{mode:terse}]', '\ReservationController:get_reservation_by_id');
     /* Get specific reservation by IP address */
-    $app->get ('/ip/{ip:[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}}', '\ReservationController:get_reservation_by_ip');
+    $app->get ('/ip/{ip:[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}}[/{mode:terse}]',
+               '\ReservationController:get_reservation_by_ip');
     /* Get all reservations for a MAC address */
-    $app->get ('/mac/{mac:(?:(?:[0-9A-Fa-f]{4}\.){2}[0-9A-Fa-f]{4}|(?:[0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2})}', '\ReservationController:get_reservation_by_mac');
+    $app->get ('/mac/{mac:(?:(?:[0-9A-Fa-f]{4}\.){2}[0-9A-Fa-f]{4}|(?:[0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2})}[/{mode:terse}]',
+               '\ReservationController:get_reservation_by_mac');
 
 });
 
