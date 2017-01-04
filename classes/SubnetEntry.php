0b1e1ac5e37edeefedfd2fc417d7dd3c59d33c45
Pavle Obradovic <pobradovic08@gmail.com>
2017-01-04 16:07:25 +0100

    SubnetEntry setters

--- classes/SubnetEntry.php
+++ classes/SubnetEntry.php
@@ -23,32 +23,12 @@ class SubnetEntry {
         if (isset($data['vlan'], $data['network'],
             $data['network_mask'], $data['subnet_description'])) {
             if (isset($data['subnet_id'])) {
-                if (Validator::validateId ($data['subnet_id'])) {
-                    $this->subnet_id = (int)$data['subnet_id'];
-                } else {
-                    throw new \InvalidArgumentException("Subnet ID is invalid");
-                }
+                $this->setId($data['subnet_id']);
             }
-            if (Validator::validateVlanId ($data['vlan'])) {
-                $this->vlan = (int)$data['vlan'];
-            } else {
-                throw new \InvalidArgumentException("Subnet ID is invalid");
-            }
-            if (Validator::validateIpAddress ($data['network'])) {
-                $this->network = ip2long ($data['network']);
-            } else {
-                throw new \InvalidArgumentException("Network address not valid");
-            }
-            if (Validator::validateIpMask ($data['network_mask'])) {
-                $this->network_mask = ip2long ($data['network_mask']);
-            } else {
-                throw new \InvalidArgumentException("Network mask is not valid");
-            }
-            if (Validator::validateDescription ($data['subnet_description'])) {
-                $this->subnet_description = $data['subnet_description'];
-            } else {
-                throw new \InvalidArgumentException("Subnet description too long");
-            }
+            $this->setVlan($data['vlan']);
+            $this->setNetwork($data['network']);
+            $this->setNetworkMask($data['network_mask']);
+            $this->setDescription($data['subnet_description']);
         } else {
             throw new \InvalidArgumentException("Missing required parameter(s)");
         }
@@ -58,14 +38,14 @@ class SubnetEntry {
          * Calculate CIDR first
          * Calculate network before fist hop, calculate broadcast before last hop
          */
-        $this->cidr = 32 - log ((ip2long ($data['network_mask']) ^ ip2long ('255.255.255.255')) + 1, 2);
-        $this->network_address = $this->calculateNetwork ();
-        $this->broadcast_address = $this->calculateBroadcast ();
-        $this->first_host_address = $this->calculateFirstHop ();
-        $this->last_host_address = $this->calculateLastHop ();
+        $this->cidr = 32 - log((ip2long($data['network_mask']) ^ ip2long('255.255.255.255')) + 1, 2);
+        $this->network_address = $this->calculateNetwork();
+        $this->broadcast_address = $this->calculateBroadcast();
+        $this->first_host_address = $this->calculateFirstHop();
+        $this->last_host_address = $this->calculateLastHop();
 
         if ($this->network != $this->network_address) {
-            throw new InvalidArgumentException("Invalid subnet address. Try with: " . long2ip ($this->network_address));
+            throw new InvalidArgumentException("Invalid subnet address. Try with: " . long2ip($this->network_address));
         }
 
     }
@@ -95,11 +75,11 @@ class SubnetEntry {
     }
 
     public function isValidHostAddress ($ip) {
-        if(Validator::validateIpAddress($ip)){
+        if (Validator::validateIpAddress($ip)) {
             $dec_ip = ip2long($ip);
             $in_subnet = (ip2long($ip) & $this->network_mask) == $this->network_address;
             return $in_subnet and
-                   (long2ip($dec_ip) != $this->getBroadcastAddress())and
+                   (long2ip($dec_ip) != $this->getBroadcastAddress()) and
                    (long2ip($dec_ip) != $this->getNetworkAddress());
         }
         return false;
@@ -109,16 +89,48 @@ class SubnetEntry {
         return $this->subnet_id;
     }
 
+    public function setId ($id) {
+        if (Validator::validateId($id)) {
+            $this->subnet_id = (int)$id;
+        } else {
+            throw new \InvalidArgumentException("Subnet ID is invalid");
+        }
+    }
+
     public function getVlan () {
         return $this->vlan;
     }
 
+    public function setVlan ($vlan_id) {
+        if (Validator::validateVlanId($vlan_id)) {
+            $this->vlan = (int)$vlan_id;
+        } else {
+            throw new \InvalidArgumentException("Subnet ID is invalid");
+        }
+    }
+
     public function getNetwork () {
-        return long2ip ($this->network);
+        return long2ip($this->network);
+    }
+
+    public function setNetwork ($network) {
+        if (Validator::validateIpAddress($network)) {
+            $this->network = ip2long($network);
+        } else {
+            throw new \InvalidArgumentException("Network address not valid");
+        }
     }
 
     public function getNetworkMask () {
-        return long2ip ($this->network_mask);
+        return long2ip($this->network_mask);
+    }
+
+    public function setNetworkMask ($mask) {
+        if (Validator::validateIpMask($mask)) {
+            $this->network_mask = ip2long($mask);
+        } else {
+            throw new \InvalidArgumentException("Network mask is not valid");
+        }
     }
 
     public function getCidr () {
@@ -126,32 +138,40 @@ class SubnetEntry {
     }
 
     public function getNetworkAddress () {
-        return long2ip ($this->network_address);
+        return long2ip($this->network_address);
     }
 
     public function getBroadcastAddress () {
-        return long2ip ($this->broadcast_address);
+        return long2ip($this->broadcast_address);
     }
 
     public function getFirstHostAddress () {
-        return long2ip ($this->first_host_address);
+        return long2ip($this->first_host_address);
     }
 
     public function getLastHostAddress () {
-        return long2ip ($this->last_host_address);
+        return long2ip($this->last_host_address);
     }
 
     public function getDescription () {
         return $this->subnet_description;
     }
 
+    private function setDescription ($subnet_description) {
+        if (Validator::validateDescription($subnet_description)) {
+            $this->subnet_description = $subnet_description;
+        } else {
+            throw new \InvalidArgumentException("Subnet description too long");
+        }
+    }
+
     public function serialize () {
         return [
-            'subnet_id' => $this->getId (),
-            'subnet_vlan' => $this->getVlan (),
-            'subnet_network' => $this->getNetwork (),
-            'subnet_mask' => $this->getNetworkMask (),
-            'subnet_description' => $this->getDescription (),
+            'subnet_id' => $this->getId(),
+            'subnet_vlan' => $this->getVlan(),
+            'subnet_network' => $this->getNetwork(),
+            'subnet_mask' => $this->getNetworkMask(),
+            'subnet_description' => $this->getDescription(),
         ];
     }
 }
