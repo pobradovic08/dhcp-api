4fad4effbb09e23543a842d50b97c6e487fadc4d
Pavle Obradovic <pobradovic08@gmail.com>
2017-01-11 13:28:32 +0100

    EndHostModel - Comments

--- classes/EndHost/EndHostModel.php
+++ classes/EndHost/EndHostModel.php
@@ -22,18 +22,30 @@ class EndHostModel extends Model {
         'production' => 'boolean',
     ];
 
+    // By default fetch type data
     protected $with = [
         'type'
     ];
 
+    /*
+     * Format MAC address as HHHH.HHHH.HHHH
+     */
     public function getMacAttribute () {
         return wordwrap(dechex($this->attributes['mac']), 4, '.', true);
     }
 
+    //TODO: setMacAttribute()
+
+    /*
+     * Get type of this host
+     */
     public function type () {
         return $this->hasOne('\Dhcp\EndHostType\EndHostTypeModel', 'end_host_type_id', 'end_host_type_id');
     }
 
+    /*
+     * Get all reservations for this host
+     */
     public function reservations () {
         return $this->hasMany('\Dhcp\Reservation\ReservationModel', 'end_host_id');
     }
