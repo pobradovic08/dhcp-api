3c6f5ce53f80ac1a8172455c5be0a7e9ba04ef9c
Pavle Obradovic <pobradovic08@gmail.com>
2017-01-13 15:04:42 +0100

    Subnet Controller - Get subnet by ID with eloquent

--- classes/Subnet/SubnetController.php
+++ classes/Subnet/SubnetController.php
@@ -43,29 +43,23 @@ class SubnetController {
         return $response->withStatus($this->r->getCode())->withJson($this->r);
     }
 
+    /*
+     * Get subnet with specified ID
+     * HTTP GET
+     */
     public function get_subnet_by_id ($request, $response, $args) {
-        $id = intval($args['subnet_id']);
-        if (!Validator::validateId($id)) {
-            $this->r->fail();
+        if (!Validator::validateArgument($args, 'subnet_id', Validator::ID)) {
+            $this->ci->logger->addError("Called " . __FUNCTION__ . "with invalid ID");
+            $this->r->fail(400, "Invalid subnet ID");
             return $response->withStatus($this->r->getCode())->withJson($this->r);
         }
         try {
-            $mapper = new SubnetMapper($this->ci->db);
-            $results = $mapper->getSubnets(['subnet_id' => $id]);
-            // Build an array of end hosts
-            if (sizeof($results) == 1) {
-                $this->r->success();
-                $this->r->setData($results[0]->serialize());
-            } else {
-                $this->r->fail();
-                $this->r->setCode(404);
-                $this->r->addMessage("Subnet with ID #{$id} not found.");
-            }
-        } catch (\InvalidArgumentException $e) {
-            $this->r->fail();
-            $this->r->addMessage($e->getMessage());
+            $result = SubnetModel::findOrFail($args['subnet_id']);
+            $this->r->success();
+            $this->r->setData($result);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            $this->r->fail(404, "Subnet with ID #{$args['subnet_id']} not found.");
         }
-        // Return response as JSON body
         return $response->withStatus($this->r->getCode())->withJson($this->r);
     }
 
