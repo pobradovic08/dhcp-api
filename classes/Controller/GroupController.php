d091ae3099545a1c6b5e0d67c1afa83309d37692
Pavle Obradovic <pobradovic08@gmail.com>
2017-01-19 16:02:52 +0100

    Group update - Controller method

--- classes/Controller/GroupController.php
+++ classes/Controller/GroupController.php
@@ -134,7 +134,67 @@ class GroupController {
 
     //TODO: PUT group
     public function put_group ($request, $response, $args) {
+        /*
+         * Check if subnet ID or group ID are valid
+         */
+        if (!Validator::validateArgument($args, 'subnet_id', Validator::REGEXP_ID)) {
+            $this->ci->logger->addError("Called " . __FUNCTION__ . "with invalid ID");
+            $this->r->fail(400, "Invalid subnet ID");
+            return $response->withStatus($this->r->getCode())->withJson($this->r);
+        }
+        if (!Validator::validateArgument($args, 'group_id', Validator::REGEXP_ID)) {
+            $this->ci->logger->addError("Called " . __FUNCTION__ . "with invalid ID");
+            $this->r->fail(400, "Invalid group ID");
+            return $response->withStatus($this->r->getCode())->withJson($this->r);
+        }
 
+        $optional_params = [
+            ['subnet_id', Validator::ID],
+            ['name', Validator::FILENAME],
+            ['description', Validator::DESCRIPTION],
+        ];
+        /*
+         * Data array used for building the Group object.
+         * Parameters from Request are filtered and copied to this array.
+         */
+        $data = [];
+
+        /*
+         * Loop through optional parameters and check if
+         * they exist and are matching the regexp defined above.
+         * No error message is generated if the parameter is missing.
+         * If the value is not matching the regexp, parameter is not
+         * added to data array.
+         */
+        foreach ($optional_params as $param) {
+            if (Validator::validateArgument($request->getParams(), $param[0], $param[1])) {
+                $data[$param[0]] = $request->getParam($param[0]);
+            }
+        }
+
+        if(empty($data)){
+            $this->r->fail(400, "No arguments passed. Nothing to update");
+        }else {
+            try {
+                $existing = GroupModel::findOrFail($args['group_id']);
+                foreach ($data as $field => $value) {
+                    $existing->$field = $value;
+                }
+                if($existing->save()){
+                    $this->r->success("Group updated");
+                    $this->r->setData($existing);
+                }else{
+                    $this->r->fail(500, "Failed to update group entry.");
+                }
+
+            }catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e){
+                $this->r->fail(404, "Group with ID {$args['group_id']} not found.");
+            }catch (\Illuminate\Database\QueryException $e) {
+                $this->r->fail(500, "Error updating group.");
+                $this->ci->logger->addError("Failed updating group: " . $e->getMessage());
+            }
+        }
+        return $response->withJson($this->r, $this->r->getCode());
     }
 
     //TODO: DELETE group
