764988587f9b1be7f54f69f6e0754730c7514130
Pavle Obradovic <pobradovic08@gmail.com>
2017-01-25 23:28:12 +0100

    Documentation

--- classes/Controller/BaseController.php
+++ classes/Controller/BaseController.php
@@ -1,9 +1,8 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: pajaja
- * Date: 1/25/2017
- * Time: 10:59 PM
+ * Base controller class that others extend
+ *
+ * @author  Pavle Obradovic <pobradovic08@gmail.com>
  */
 
 namespace Dhcp\Controller;
@@ -12,9 +11,22 @@ use \Interop\Container\ContainerInterface as ContainerInterface;
 use Dhcp\Response;
 
 class BaseController {
+
+    /**
+     * Container interfaces provided by Slim framework
+     *
+     * @var ContainerInterface
+     */
     protected $ci;
 
-    //Constructor
+    /**
+     * Contains function that all Controllers have:
+     * - Sets the local Container Interface
+     * - Initializes Elloquent Capsule (Database)
+     * - Creates custom Response instance
+     *
+     * @param ContainerInterface $ci
+     */
     public function __construct (ContainerInterface $ci) {
         $this->ci = $ci;
         $this->ci->capsule;
