48e9b97be208a9370bafabfafe5a1b7df17210a1
Pavle Obradovic <pobradovic08@gmail.com>
2017-01-26 19:56:49 +0100

    PHPDocs

--- classes/Controller/EndHostTypeController.php
+++ classes/Controller/EndHostTypeController.php
@@ -16,9 +16,11 @@ use Psr\Http\Message\ResponseInterface;
 class EndHostTypeController extends BaseController {
 
     /**
-     * @param ServerRequestInterface $request
+     * Returns all endhost types
+     *
+     * @param ServerRequestInterface $request Not used
      * @param ResponseInterface $response
-     * @param array $args
+     * @param array $args Not used
      * @return ResponseInterface
      */
     public function get_type (ServerRequestInterface $request, ResponseInterface $response, $args) {
@@ -29,20 +31,26 @@ class EndHostTypeController extends BaseController {
     }
 
     /**
-     * @param ServerRequestInterface $request
+     * Returns single endhost type by ID
+     *
+     * @param ServerRequestInterface $request Not used
      * @param ResponseInterface $response
-     * @param array $args
+     * @param array $args Should contain 'end_host_type_id' key
      * @return ResponseInterface
      */
     public function get_type_by_id (ServerRequestInterface $request, ResponseInterface $response, $args) {
-        // API Response
+        /*
+         * Validates 'end_host_type_id' route argument
+         */
         if (!Validator::validateArgument($args, 'end_host_type_id', Validator::REGEXP_ID)) {
             $this->ci->logger->addError("Called " . __FUNCTION__ . "with invalid ID");
             $this->r->fail(400, "Invalid host type ID");
             return $response->withStatus($this->r->getCode())->withJson($this->r);
         }
+        /*
+         * Fetch endhost type or fail with 404 status
+         */
         $type = EndHostTypeModel::find($args['end_host_type_id']);
-        // Prepare API response
         if ($type) {
             $this->r->success();
             $this->r->setData($type);
@@ -53,16 +61,28 @@ class EndHostTypeController extends BaseController {
     }
 
     /**
-     * @param ServerRequestInterface $request
+     * Create new endhost type.
+     * Required fields:
+     *  - Description
+     *
+     * @param ServerRequestInterface $request Should contain 'description' parameter
      * @param ResponseInterface $response
-     * @param array $args
+     * @param array $args Not used
      * @return ResponseInterface
      */
     public function create_type (ServerRequestInterface $request, ResponseInterface $response, $args) {
+        //TODO: refactor code with try/catch and messages
+        /*
+         * Validate 'description' parameter
+         */
         if (!Validator::validateArgument($request->getParams(), 'description', Validator::DESCRIPTION)) {
             $this->r->fail(400, "Required parameter(s) missing or invalid");
             return $response->withStatus($this->r->getCode())->withJson($this->r);
         } else {
+            /*
+             * Create new endhost type with provided parameters
+             * and save it to database
+             */
             $type = new EndHostTypeModel([
                                              'description' => $request->getParam('description')
                                          ]);
@@ -77,12 +97,17 @@ class EndHostTypeController extends BaseController {
     }
 
     /**
+     * Updates endhost type with ID
+     * Editable fields:
+     *  - Description
+     *
      * @param ServerRequestInterface $request
      * @param ResponseInterface $response
      * @param array $args
      * @return ResponseInterface
      */
     public function update_type (ServerRequestInterface $request, ResponseInterface $response, $args) {
+        //TODO: Refactor code with argument array and loop. Add try/catch block
         if (
             !Validator::validateArgument($request->getParams(), 'description', Validator::DESCRIPTION) ||
             !Validator::validateArgument($args, 'end_host_type_id', Validator::ID)
@@ -90,6 +115,10 @@ class EndHostTypeController extends BaseController {
             $this->r->fail(400, "Required parameter(s) missing or invalid");
             return $response->withJson($this->r, $this->r->getCode());
         } else {
+            /*
+             * Create new endhost type and save it to database
+             */
+            //TODO: no need to create if it doesn't exists. Throw 404 on missing ID
             $type = EndHostTypeModel::firstOrCreate([
                                                         'end_host_type_id' => $args['end_host_type_id']
                                                     ]);
@@ -105,26 +134,34 @@ class EndHostTypeController extends BaseController {
     }
 
     /**
-     * @param ServerRequestInterface $request
+     * Delete endhost type by ID
+     *
+     * @param ServerRequestInterface $request Not used
      * @param ResponseInterface $response
-     * @param array $args
+     * @param array $args Should contain 'end_host_type_id' key
      * @return ResponseInterface
      */
     public function delete_type (ServerRequestInterface $request, ResponseInterface $response, $args) {
-        // API Response
+        /*
+         * Validate 'end_host_type_id' route argument
+         */
         if (!Validator::validateArgument($args, 'end_host_type_id', Validator::REGEXP_ID)) {
             $this->ci->logger->addError("Called " . __FUNCTION__ . "with invalid ID");
             $this->r->fail(400, "Invalid host type ID");
             return $response->withJson($this->r, $this->r->getCode());
         }
+        /*
+         * Find endhost type by ID and delete it.
+         * Fail with 404 if ID doesn't exists
+         * Fail with 500 if couldn't delete
+         */
         try {
             $type = EndHostTypeModel::findOrFail($args['end_host_type_id']);
-            // Prepare API response
             if ($type->delete()) {
                 $this->r->success('Host type ' . $type->description . ' deleted');
                 $this->ci->logger->addInfo("Deleted end host type #" . $args['end_host_type_id']);
             } else {
-                $this->r->fail(404);
+                $this->r->fail(505);
             }
         }catch (ModelNotFoundException $e){
             $this->r->fail(404, "Host type #{$args['end_host_type_id']} not found");
